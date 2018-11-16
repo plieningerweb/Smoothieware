94c2662264c6acf04e338376c7dae68e906d5b1c
Andreas Plieninger <plieningerweb@gmail.com>
2018-11-16 15:57:44 +0100

    fix: register module in main

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@
 #include "modules/utils/killbutton/KillButton.h"
 #include "modules/utils/PlayLed/PlayLed.h"
 #include "modules/utils/panel/Panel.h"
+#include "modules/utils/queuesize/QueueSize.h"
 #include "libs/Network/uip/Network.h"
 #include "Config.h"
 #include "checksumm.h"
@@ -189,6 +190,10 @@ void init() {
     #ifndef NO_UTILS_MOTORDRIVERCONTROL
     kernel->add_module( new MotorDriverControl(0) );
     #endif
+    #ifndef NO_UTILS_QUEUESIZE
+    kernel->add_module( new(AHB0) QueueSize() );
+    #endif
+
     // Create and initialize USB stuff
     u.init();
 
--- src/modules/utils/queuesize/QueueSize.h
+++ src/modules/utils/queuesize/QueueSize.h
@@ -13,70 +13,20 @@ to cool the hot end's cold zone. Specifically, it turns one of the small MOSFETs
 Author: Michael Hackney, mhackney@eclecticangler.com
 */
 
-#ifndef TEMPERATURESWITCH_MODULE_H
-#define TEMPERATURESWITCH_MODULE_H
+#ifndef QUEUESIZE_MODULE_H
+#define QUEUESIZE_MODULE_H
 
 using namespace std;
 
 #include "libs/Module.h"
-#include <string>
-#include <vector>
 
 class QueueSize : public Module
 {
     public:
         QueueSize();
-        ~QueueSize();
+        virtual ~QueueSize() {};
         void on_module_loaded();
-        void on_second_tick(void *argument);
         void on_gcode_received(void *argument);
-        QueueSize* load_config(uint16_t modcs);
-
-        bool is_armed() const { return armed; }
-
-    private:
-        enum TRIGGER_TYPE {LEVEL, RISING, FALLING};
-        enum STATE {NONE, HIGH_TEMP, LOW_TEMP};
-
-        // get the highest temperature from the set of configured temperature controllers
-        float get_highest_temperature();
-
-        // turn the switch on or off
-        void set_switch(bool cooler_state);
-
-        // temperature has changed state
-        void set_state(STATE state);
-
-        // temperatureswitch.hotend.threshold_temp
-        float temperatureswitch_threshold_temp;
-
-        // temperatureswitch.hotend.switch
-        uint16_t temperatureswitch_switch_cs;
-
-        // check temps on heatup every X seconds
-        // this can be set in config: temperatureswitch.hotend.heatup_poll
-        uint16_t temperatureswitch_heatup_poll;
-
-        // check temps on cooldown every X seconds
-        // this can be set in config: temperatureswitch.hotend.cooldown_poll
-        uint16_t temperatureswitch_cooldown_poll;
-
-        // our internal second counter
-        uint16_t second_counter;
-
-        // we are delaying for this many seconds
-        uint16_t current_delay;
-
-        // the mcode that will arm the switch, 0 means always armed
-        uint16_t arm_mcode;
-
-        struct {
-            char designator:8;
-            bool inverted:1;
-            bool armed:1;
-            TRIGGER_TYPE trigger:2;
-            STATE current_state:2;
-        };
 };
 
 #endif
